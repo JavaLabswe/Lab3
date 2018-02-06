f7db8382f5e5e13bdfd8918e6fbef2bef726472c
Arifeen Kundee <feenkundee@gmail.com>
2018-02-06 09:32:08 +0700

    Update

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//
-		System.out.print("Hello");
+		Scanner in = new Scanner(System.in);
+		int n;
+		n = in.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+				System.out.print("A"+" ");
+			}
+			System.out.println("");
+		}
 
 	}
 
