df4fa50aa0c000e89aa30ee4c2476bb6e427dfa5
DESKTOP-AMCAV7I\Msi <s5211_12@hotmail.com>
2018-02-06 09:47:09 +0700

    การบ้าน สัปดาห์8 ข้อ 1

--- src/loop0.java
+++ src/loop0.java
@@ -1,8 +1,19 @@
-
+import java.util.Scanner;
 public class loop0 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner inp = new Scanner(System.in);
+		int n,i,j;
+		n = inp.nextInt();
+		for(i=0;i<n;i++) {
+			for(j=0;j<=i;j++) {
+				System.out.print("A");
+			}
+			System.out.println();
+		}
+		
+		
 
 	}
 
