a66e05bcd3daab97412411685f9ae5a9d36d4789
sunsunnp <sunsun.np@gmail.com>
2018-02-06 09:03:55 +0700

    Testpush

---
+++ src/Loop1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Loop1 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("N : ");
+		int n = in.nextInt();
+		
+		for(int i=1 ; i<n ; i++) {
+			System.out.println("A");
+		}
+	}
+
+}
