224fdef4c59bb12894e56e0cbcc83c847d2abf1c
DESKTOP-AMCAV7I\Msi <s5211_12@hotmail.com>
2018-02-06 11:55:22 +0700

    การบ้านสัปดาห์ที่8 ข้อ 2

---
+++ src/loop1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class loop1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner inp = new Scanner(System.in);
+		int n,i,j;
+		n = inp.nextInt();
+		for(i=0;i<n;n--) {
+			for(j=0;j<n;j++) {
+				System.out.print("A");
+			}
+			System.out.println();
+		}
+		
+		
+
+	}
+
+}
+
