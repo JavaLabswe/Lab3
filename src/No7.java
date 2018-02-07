2a8f21ac1e756764922b965a4cfbe22e15be4c94
aeakwit001 <aakawit_13@hotmail.co.th>
2018-02-08 01:34:55 +0700

    ส่งข้อ7

---
+++ src/No7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class No7 {
+
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+		int m =in.nextInt();
+		for(int i=1 ;i<=m;i++) {
+			for(int j=1 ;j<=m ;j++) {
+				if(i==1 || j==1 || i==m || j==m) {
+					System.out.print('*');	
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println("");
+		}
+	}
+}
