d7afca849ddc8669604098ac63232d28e150f647
aeakwit001 <aakawit_13@hotmail.co.th>
2018-02-08 02:53:57 +0700

    ส่งข้อ12

---
+++ src/No12.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class No12 {
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+		int m =in.nextInt();
+		for(int i=m;i>0;i--) {
+			for(int k=i;k<m;k++) {
+				System.out.print(" ");
+			}
+			for(int j=i;j>=1;j--) {
+				System.out.print('*');
+			}
+			for(int j=i;j>1;j--) {
+				System.out.print('*');
+			}
+			for(int j=i;j<m;j++) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
