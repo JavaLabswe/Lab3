682b4e7b32a8bdfbeb92289cb117788295fe7ef9
Suppatsorn <Suppatsorn@172.18.134.128>
2018-02-08 21:27:37 +0700

    update

---
+++ src/No12.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class No12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				Scanner in =new Scanner(System.in);
+				int m =in.nextInt();
+				for(int i=m;i>0;i--) {
+					for(int k=i;k<m;k++) {
+						System.out.print(" ");
+					}
+					for(int j=i;j>=1;j--) {
+						System.out.print('*');
+					}
+					for(int j=i;j>1;j--) {
+						System.out.print('*');
+					}
+					for(int j=i;j<m;j++) {
+						System.out.print(" ");
+					}
+					System.out.println();
+				}
+			}
+		}
